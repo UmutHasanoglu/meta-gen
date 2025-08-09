18dca0b6f9ff0aef245c0570cd8881090d5c53a2
UmutHasanoglu <hasanovumut@gmail.com>
2025-08-09 15:26:55 +0300

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen bg-neutral-950 text-neutral-100 antialiased">{children}</body>
+      <body className="min-h-screen bg-neutral-950 text-neutral-100 antialiased">{children}<Analytics /></body>
     </html>
   );
 }
